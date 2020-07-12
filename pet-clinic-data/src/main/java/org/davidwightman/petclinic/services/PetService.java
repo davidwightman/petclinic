b55aa22266942f9b81618e12373f52fd98f5eb37
David Wightman <david@davidwightman.org>
2020-07-12 15:41:03 -0400

    add pet and specialty jpa services

---
+++ pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/PetService.java
@@ -0,0 +1,6 @@
+package org.davidwightman.petclinic.services;
+
+import org.davidwightman.petclinic.model.Pet;
+
+public interface PetService extends CrudService<Pet, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package org.davidwightman.petclinic.services;
+
+import org.davidwightman.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long>{
+
+}
