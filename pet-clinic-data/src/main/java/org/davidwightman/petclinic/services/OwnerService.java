2af363873d4d90b9f9646823ace6509fbda8ce32
David Wightman <david@davidwightman.org>
2020-07-12 14:40:09 -0400

    spring data jpa service for owners

---
+++ pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package org.davidwightman.petclinic.services;
+
+import org.davidwightman.petclinic.model.Owner;
+
+import java.util.List;
+
+public interface OwnerService extends CrudService<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
+}
