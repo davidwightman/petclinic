bccb9090d53bc207580439634764fae1441e5e00
David Wightman <david@davidwightman.org>
2020-07-12 15:18:03 -0400

    add petType and vet jpa services

---
+++ pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package org.davidwightman.petclinic.services;
+
+import org.davidwightman.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/VetService.java
@@ -0,0 +1,7 @@
+package org.davidwightman.petclinic.services;
+
+import org.davidwightman.petclinic.model.Vet;
+
+public interface VetService extends CrudService<Vet, Long>{
+
+}
