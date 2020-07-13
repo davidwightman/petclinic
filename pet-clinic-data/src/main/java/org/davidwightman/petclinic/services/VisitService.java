ed19afcc7285ad32d15cef106b61935958870946
David Wightman <david@davidwightman.org>
2020-07-13 07:43:23 -0400

    add visit jpa service and DataLoader

---
+++ pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package org.davidwightman.petclinic.services;
+
+import org.davidwightman.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/davidwightman/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package org.davidwightman.petclinic.services.map;
+
+import org.davidwightman.petclinic.model.Owner;
+import org.davidwightman.petclinic.model.Visit;
+import org.davidwightman.petclinic.services.OwnerService;
+import org.davidwightman.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
